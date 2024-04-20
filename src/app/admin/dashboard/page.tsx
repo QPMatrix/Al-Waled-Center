2eb82f4362a7c885771608daed50dbe96ed64338
QPM <admin@QuantumPinnacleMatrix.tech>
2024-04-20 19:43:05 +0300

    forxcing re deploy

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = async () => {
-  return <div className="flex items-center justify-center min-h-screen"></div>;
+  return <div className="flex justify-center min-h-screen"></div>;
 };
 
 export default Page;
